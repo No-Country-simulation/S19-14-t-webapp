918352a7538affa8c64166ee1743333c2d190a7f
Ariel <ariel.nicolas.aguilar@gmail.com>
2024-11-28 15:42:17 -0300

    Revert "Merge branch 'dev' of https://github.com/No-Country-simulation/s19-14-t-webapp into ariel"
    This reverts commit e4ac37fee7634ba590422e07798e2cc56db5e59b, reversing
    changes made to 7da25623c6ca32e6c583844dbe84ff7bd925a5c4.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from './modules/home/pages/Home'
-import Registro from './modules/auth/pages/Registro';
-import { Footer } from "./core/components/Footer";
-import { Landing } from "./modules/landing/pages/Landing";
+import { Landing } from "./modules/landing/pages/Landing"
 
-export default function App() {
+function App() {
     return (
-      <>
-      <Router>
-        <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/registro" element={<Registro/>} />
-            <Route path="/landing" element={<Landing/>} />
-
-        </Routes>
-      </Router>
-      <Footer />
-      </>
-    )
+        <Landing />
+)
 }
+
+export default App
