252c68086ad944ffaf04d47e1796384e6ef34dd2
Ariel <ariel.nicolas.aguilar@gmail.com>
2024-12-05 14:46:57 -0300

    Add navs & fix styles

--- frontend/src/core/components/Navbar.jsx
+++ frontend/src/core/components/Navbar.jsx
@@ -5,11 +5,11 @@ export const Navbar = () => {
     return (
         <nav className={styles["navbar"]}>
             <div className={styles["navbar-nav"]}>
-                <a href="#" className={styles["nav-logo"]}><img src={logo} alt="logo"/> OficiosYa</a>
+                <a href="/" className={styles["nav-logo"]}><img src={logo} alt="logo"/> OficiosYa</a>
                 <ul className={styles["nav-items"]}>
-                    <li><a href="#" className={styles["nav-link"]}>Profesionales</a></li>
-                    <li><a href="#" className={styles["nav-link"]}>Sobre nosotros</a></li>
-                    <li><a href="#" className={styles["nav-button"]}>Iniciar sesión / Registrarse</a></li>
+                    <li><a href="/" className={styles["nav-link"]}>Profesionales</a></li>
+                    <li><a href="/nosotros" className={styles["nav-link"]}>Sobre nosotros</a></li>
+                    <li><a href="/registro" className={styles["nav-button"]}>Iniciar sesión / Registrarse</a></li>
                 </ul>
             </div>
             {/* <div className="navbar-menuBtn"></div> */}
