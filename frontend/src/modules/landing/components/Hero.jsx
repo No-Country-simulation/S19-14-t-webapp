bb8bd3385e5a61017fd55b6dc793d8778381ccf9
Ariel <ariel.nicolas.aguilar@gmail.com>
2024-12-09 11:10:21 -0300

    add route navs & fix styles

--- frontend/src/modules/landing/components/Hero.jsx
+++ frontend/src/modules/landing/components/Hero.jsx
@@ -8,8 +8,8 @@ export const Hero = () => {
                 <p className={styles["hero-text"]}><b>OficiosYa</b> facilita la conexión entre clientes y profesionales de confianza en servicios como electricidad, plomería, jardinería, etc. Nuestra plataforma te permite encontrar expertos calificados, revisar su experiencia y contactarlos directamente para el servicio que necesitas. Cada profesional ha sido verificado, asegurando rapidez y seguridad en cada proyecto. ¡Simplifica tus búsquedas con OficiosYa!</p>
             </div>
             <div className={styles["hero-buttons"]}>
-                <a href="#" className={styles["hero-btn"]}>Buscar un servicio</a>
-                <a href="#" className={styles["hero-btn"]}>Ofrecer un servicio</a>
+                <a href="/" className={styles["hero-btn"]}>Buscar un servicio</a>
+                <a href="/registro" className={styles["hero-btn"]}>Ofrecer un servicio</a>
             </div>
         </section>
     )
