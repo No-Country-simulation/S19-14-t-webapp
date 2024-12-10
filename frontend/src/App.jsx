10709e7f9a85e7ea5e25df87c6d772c0c3223657
fabricio <fabricio.cbe@gmail.com>
2024-12-10 14:36:24 -0300

    img perfil

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,9 @@ import Home from './modules/home/pages/Home'
 import Registro from './modules/auth/pages/Registro';
 import { Footer } from "./core/components/Footer";
 import { Landing } from "./modules/landing/pages/Landing";
-
 import Login from "./modules/auth/pages/Login";
 import { Navbar } from "./core/components/Navbar";
-
+import Profilepage from "./modules/landing/pages/ProfilePage/ProfilePage"
 
 export default function App() {
     return (
@@ -17,7 +16,7 @@ export default function App() {
             <Route path="/" element={<Home/>} />
             <Route path="/registro" element={<Registro/>} />
             <Route path="/login" element={<Login/>} />       
-
+            <Route path="/profile" element={<Profilepage/>} />    
             <Route path="/nosotros" element={<Landing/>} />
 
         </Routes>
